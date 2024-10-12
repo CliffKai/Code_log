c0ad83783eb11f2667d4cf18e292a6ab9d3d11f6
CliffKai <cliffkai@CliffKaideMacBook-Air.local>
2024-10-12 18:09:05 +0800

    3158

---
+++ 3158.cpp
@@ -0,0 +1,27 @@
+#include<iostram>
+
+using namespace std;
+
+int main()
+{
+    vector<int> nums = {4,3,2,7,8,2,3,1};
+    cout << duplicateNumbersXOR(nums) << endl;
+    return 0;
+}
+
+int duplicateNumbersXOR(vector<int>& nums) {
+    int ans = 0;
+    long long hash = 0;
+    for(int i : nums)
+    {
+        if(hash >> i & 1)
+        {
+            ans ^= i;
+        }
+        else
+        {
+            hash |= 1LL << i;
+        }
+    }
+    return ans;
+}
