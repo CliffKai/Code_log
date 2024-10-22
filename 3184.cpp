e199f9ca4b5134a6ca043eee28f9ab9ffb900b9c
CliffKai <cliffkai@CliffKaideMacBook-Air.local>
2024-10-22 09:14:25 +0800

    3194 & 3196

---
+++ 3184.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int countCompleteDayPairs(vector<int>& hours) {
+    vector<int> hash(24,0);
+    int n = hours.size(),ans = 0;
+    for(int i = 0;i < n;++ i)
+    {
+        ++ hash[hours[i] % 24];
+    }
+    for(int i = 1;i < 12;++ i)
+    {
+        ans += hash[i] * (hash[24 - i]);
+    }
+    ans += (hash[0] * (hash[0] - 1)) / 2;
+    ans += (hash[12] * (hash[12] - 1)) / 2;
+    return ans;
+}
+
+int main()
+{
+    vector<int> hours = {9,9,9,9,9,9,9,9,9,9};
+    cout << countCompleteDayPairs(hours) << endl;
+    return 0;
+}
